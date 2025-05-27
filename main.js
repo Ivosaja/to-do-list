6b53b5e98464ca0024527ce4932c6e4ef6c81a1f
Ivosaja <ivoliobel@gmail.com>
2025-05-27 01:14:43 -0300

    add to-do tasks and some documentation

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+////////////////////////
+/// GLOBAL VARIABLES ///
 const inputTask = document.getElementById('taskInput');
 const tasksList = document.querySelector('.tasks-list');
 let arrayTask = [];
 let tasks = '';
 let counter = 0;
 
-
+///////////////////////////////////
+////// Function to show tasks /////
 function showTasks(arrayTasks){
     let tasks = '';
     for(let i = 0; i < arrayTasks.length; i++){
@@ -13,6 +16,8 @@ function showTasks(arrayTasks){
     tasksList.innerHTML = tasks;
 };
 
+///////////////////////////////////////////////////////
+////// Function to add a task to the to-do list ///////
 function addTask(){
     inputTask.addEventListener('keyup', (e) => {
         if(e.key === 'Enter'){
@@ -25,16 +30,22 @@ function addTask(){
     
 };
 
+/////////////////////////////////////////////////////
+//////// Function to mark a task as completed ///////
 function hightlightAsCompleted(idCheckbox){
+    // TODO : Finish the highlight function to mark a task as completed
     const itemTask = arrayTask.find(task => idCheckbox === task.id);
 
 }
 
+//////////////////////////////////////////////////////////////
+//////// Function to delete a task from the to-do list ///////
 function deleteTask(){
-
+    // TODO: Finish the delete function to delete whatever task you want
 }
 
-
+/////////////////////////////////////////////////
+//// Function to initialize some functions //////
 function init(){
     addTask();
 }
